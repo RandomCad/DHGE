952cd171db7f4b1d0569987036b14b9cf383dd49
RandomCad <mmont4850@gmail.com>
2024-04-26 14:24:53 +0200

    Entprellter taster
    Signed-off-by: RandomCad <mmont4850@gmail.com>

--- togel.asm
+++ togel.asm
@@ -5,6 +5,7 @@
     .def  tast = r18;
     SBI   DDRD, 7     ; set 7 bit -> pin 7 from port D set as output
     CBI   DDRB, 4     ; Clear the 4 bit -> pin 12 is input
+    SBI   PORTB, 4     ; Clear the 4 bit -> pin 12 is input
     ldi   work, 0x80   ; Set LED to on
     ldi togleMask, 0x80
 start:
@@ -13,11 +14,18 @@ start:
   rjmp start
 
 togglePin:
-  SBIS PINB, 4;
-  rjmp togglePin;
-
+; entprellen
+  LDI R25, HIGH(16000)
+  LDI R24, LOW(16000)
+  entprellen:
+    SBIW R24, 1
+    BRNE entprellen
+  SBIC PINB, 4;
+    ret;
   IN work, PORTD
   EOR work, togleMask;
   out PORTD, work
-
+stuck:
+  SBIS PINB, 4;
+    rjmp stuck;
   ret
